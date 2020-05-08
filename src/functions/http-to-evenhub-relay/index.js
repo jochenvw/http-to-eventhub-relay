9f603fc87049b5703884f8e2a8473f82498d534a
Jochen van Wylick <jowylick@microsoft.com>
2020-05-08 21:37:48 +0200

    Initial code for node function

---
+++ src/functions/http-to-evenhub-relay/index.js
@@ -0,0 +1,31 @@
+module.exports = async function (context, req) {
+    const { EventHubProducerClient } = require("@azure/event-hubs");
+
+    const eventHubbConnectionString = process.env["EVENTHUB_CONNECTION_STRING"];
+    const eventHub = process.env["EVENTHUB"];
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        const producerClient = new EventHubProducerClient(eventHubbConnectionString, eventHub);
+        const eventDataBatch = await producerClient.createBatch();
+        let wasAdded = eventDataBatch.tryAdd({ body: req.body });
+        if (!wasAdded) {
+            break;
+        }
+
+        await producerClient.sendBatch(eventDataBatch);
+        await producerClient.close();
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Message posted to queue !"
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a valid body in the request"
+        };
+    }
+};
